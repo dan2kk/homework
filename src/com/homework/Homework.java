4b2c7b9cb2db2a525e6c89359b0b88810dc08e62
dan2kk <dan2kk@naver.com>
2023-07-20 10:56:29 +0900

    change to sqlite DB

---
+++ src/com/homework/Homework.java
@@ -0,0 +1,100 @@
+package com.homework;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+public class Homework {
+    public static ArrayList<Integer> findIdx(String addr){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        while(addr.length() != 0){
+            int roLastIdx = addr.lastIndexOf("로");
+            int gilLastIdx = addr.lastIndexOf("길");
+            if(roLastIdx > gilLastIdx) {
+                result.add(roLastIdx);
+                addr = addr.substring(0, roLastIdx);
+            }
+            else if(roLastIdx < gilLastIdx){
+                result.add(gilLastIdx);
+                addr = addr.substring(0, gilLastIdx);
+            }
+            else{
+                break;
+            }
+        }
+        return result;
+    }
+    public static Map<String, String> findRegion(List<Object> resultList, String addr){
+        if(resultList.size() == 1)
+            return (Map)resultList.get(0);
+        for(int j=addr.length()-1; j != -1; j--){
+            //System.out.println(result);
+            if(resultList.size() == 1)
+                break;
+            List<Object> tempList = new ArrayList<Object>(resultList);
+            //System.out.println(addr.substring(j, j+1)); => 한글자씩 파싱하여 기초자치단체 혹은 광역자치단체에 있는지 확인
+            for (Object o : resultList) {
+                Map<String, String> temp = (Map) o;
+                if (!temp.get("광역자치단체").contains(addr.substring(j, j + 1)) && !temp.get("기초자치단체").contains(addr.substring(j, j + 1))) {
+                    tempList.remove(temp);
+                }
+            }
+            resultList = new ArrayList<Object>(tempList);
+        }
+        if(resultList.isEmpty())
+            return null;
+        else
+            return (Map)resultList.get(0);
+        /*
+        if(result.size() == 1){
+            return (Map)result.get(0);
+        }
+        else{
+            for(int j=1; j< addr.length(); j++){
+                for(int i=0; i< result.size(); i++){
+                    Map<String, String> temp = (Map)result.get(i);
+                    System.out.println(addr.substring(0, j));
+                    if(temp.get("광역자치단체").contains(addr.substring(0, j)))
+                        return temp;
+                    else if(temp.get("기초자치단체").contains(addr.substring(0, j)))
+                        return temp;
+                }
+            }
+            return null;
+        }
+         */
+    }
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        final String dbURL = "jdbc:sqlite:address.db";
+
+        String addr = s.nextLine();
+        String newAddr = addr.replaceAll("[^가-힣0-9]", "");
+        ArrayList<Integer> idxList = findIdx(newAddr);
+        //System.out.println(newAddr);
+        //System.out.println(findIdx(newAddr));
+        //System.out.println(idxList);
+        SqlConn sql = new SqlConn(dbURL);
+        sql.setDbConn();
+
+        List<Object> result;
+        for(int i = 0; i!= idxList.size(); i++){
+            for(int j=0; j<idxList.get(i); j++){
+                //System.out.println(newAddr.substring(j, idxList.get(i)+1));
+                result = sql.selectAddress(newAddr.substring(j, idxList.get(i)+1));
+                if(!result.isEmpty()){
+                    System.out.println(result);
+                    Map<String, String> temp = findRegion(result, newAddr.substring(0, j));
+                    if(temp == null){
+                        System.out.println("옳바르지 않은 주소 입니다.");
+                        sql.closeDbConn();
+                        return;
+                    }
+                    System.out.println(temp.get("광역자치단체")+" "+temp.get("기초자치단체")+" "+temp.get("도로명주소"));
+                    sql.closeDbConn();
+                    return;
+                }
+            }
+        }
+        System.out.println("옳바르지 않은 주소입니다");
+    }
+}
---
+++ src/com/homework/SqlConn.java
@@ -0,0 +1,63 @@
+package com.homework;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SqlConn {
+    private final String  url;
+    private Connection dbConn;
+    public SqlConn(String u){
+        this.url = u;
+    }
+    public void setDbConn(){
+        try
+        {
+            dbConn = DriverManager.getConnection(url);
+            //System.out.println("DB Connection [성공]");
+        }
+        catch (SQLException e)
+        {
+            System.out.println("DB Connection [실패]");
+            e.printStackTrace();
+        }
+    }
+    public void closeDbConn(){
+        try
+        {
+            if(dbConn != null)
+            {
+                dbConn.close();
+                dbConn = null;
+                //System.out.println("DB Close [성공]");
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println("DB Close [실패]");
+            e.printStackTrace();
+        }
+    }
+    public List<Object> selectAddress(String addr){
+        PreparedStatement st;
+        List<Object> resultList = new ArrayList<>();
+        try{
+            st = dbConn.prepareStatement("select * from Address where 도로명주소 = ?");
+            st.setString(1, addr);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Map<String, String> tempMap = new HashMap<>();
+                for (int i = 0; i < rs.getMetaData().getColumnCount(); i++)
+                    tempMap.put(rs.getMetaData().getColumnName(i + 1), rs.getString(rs.getMetaData().getColumnName(i + 1)));
+                resultList.add(tempMap);
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        return resultList;
+    }
+
+}
