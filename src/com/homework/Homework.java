5d2e9b8cd2fa29813823e0d2be921a542da82057
dan2kk <dan2kk@naver.com>
2023-07-20 14:48:33 +0900

    changing code more clearly

--- src/com/homework/Homework.java
+++ src/com/homework/Homework.java
@@ -4,11 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 public class Homework {
-    public static String safeGet(String s){
-        if(s ==null){
-            return "";
+    public static Boolean findSQL(SqlConn sql, String newAddr, int j, int idx){
+        List<Object> result;
+        result = sql.selectAddress(newAddr.substring(j, idx));
+        if(!result.isEmpty()){
+            System.out.println(result);
+            Map<String, String> temp = findRegion(result, newAddr.substring(0, j));
+            if(temp == null){
+                System.out.println("옳바르지 않은 주소 입니다.");
+                sql.closeDbConn();
+                return true;
+            }
+            System.out.println(safeGet(temp.get("광역자치단체"))+" "+safeGet(temp.get("기초자치단체"))+" "+safeGet(temp.get("도로명주소")));
+            return true;
         }
-        else return s;
+        return false;
+    }
+    public static String safeGet(String s) {
+        return s == null ? "" : s;
     }
     public static ArrayList<Integer> findIdx(String addr){
         ArrayList<Integer> result = new ArrayList<Integer>();
@@ -46,60 +59,31 @@ public class Homework {
             }
             resultList = new ArrayList<Object>(tempList);
         }
-        if(resultList.isEmpty())
-            return null;
-        else
-            return (Map)resultList.get(0);
-        /*
-        if(result.size() == 1){
-            return (Map)result.get(0);
-        }
-        else{
-            for(int j=1; j< addr.length(); j++){
-                for(int i=0; i< result.size(); i++){
-                    Map<String, String> temp = (Map)result.get(i);
-                    System.out.println(addr.substring(0, j));
-                    if(temp.get("광역자치단체").contains(addr.substring(0, j)))
-                        return temp;
-                    else if(temp.get("기초자치단체").contains(addr.substring(0, j)))
-                        return temp;
-                }
-            }
-            return null;
-        }
-         */
+        return resultList.isEmpty() ? null : (Map)resultList.get(0);
     }
     public static void main(String args[]){
-        final String dbURL = "jdbc:sqlite:address.db";
         Scanner s = new Scanner(System.in);
-        String addr = s.nextLine();
-        String newAddr = addr.replaceAll("[^가-힣0-9]", "");
-        ArrayList<Integer> idxList = findIdx(newAddr);
-        //System.out.println(newAddr);
-        //System.out.println(findIdx(newAddr));
-        //System.out.println(idxList);
+        final String dbURL = "jdbc:sqlite:address.db";
         SqlConn sql = new SqlConn(dbURL);
         sql.setDbConn();
 
-        List<Object> result;
+        System.out.println("주소를 입력해주세요");
+        String addr = s.nextLine();
+
+        String newAddr = addr.replaceAll("[^가-힣A-Za-z·\\d~\\.]", "");
+        newAddr = newAddr.toUpperCase();
+        ArrayList<Integer> idxList = findIdx(newAddr);
+        System.out.println(newAddr);
+        //System.out.println(findIdx(newAddr));
+        //System.out.println(idxList);
         for(int i = 0; i!= idxList.size(); i++){
             for(int j=0; j<idxList.get(i); j++){
-                //System.out.println(newAddr.substring(j, idxList.get(i)+1));
-                result = sql.selectAddress(newAddr.substring(j, idxList.get(i)+1));
-                if(!result.isEmpty()){
-                    System.out.println(result);
-                    Map<String, String> temp = findRegion(result, newAddr.substring(0, j));
-                    if(temp == null){
-                        System.out.println("옳바르지 않은 주소 입니다.");
-                        sql.closeDbConn();
-                        return;
-                    }
-                    System.out.println(safeGet(temp.get("광역자치단체"))+" "+safeGet(temp.get("기초자치단체"))+" "+safeGet(temp.get("도로명주소")));
+                System.out.println(newAddr.substring(j, idxList.get(i)+1));
+                if(findSQL(sql, newAddr, j, idxList.get(i)+1)){
                     sql.closeDbConn();
                     return;
                 }
             }
         }
-        System.out.println("옳바르지 않은 주소입니다");
     }
 }
